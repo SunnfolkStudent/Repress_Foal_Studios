9be2fb09f9384728a7afed7a823fec3555d0ce02
Lacrimosa-GH <lacrimosa1457@gmail.com>
2026-04-10 10:35:01 +0200

    Fmod added

--- Assets/Scripts/Challenges/PatrolMovement.cs
+++ Assets/Scripts/Challenges/PatrolMovement.cs
@@ -10,20 +10,27 @@ public class PatrolMovement : MonoBehaviour
     private Transform[] waypoints;
     private int currentWaypointIndex;
     private bool isWaiting;
+    private Animator _animator;
 
     void Start()
     {
+        
+        _animator =  gameObject.GetComponent<Animator>();
         waypoints = new  Transform[waypointParent.childCount];
 
         for (int i = 0; i < waypointParent.childCount; i++)
         {
             waypoints[i] = waypointParent.GetChild(i);
         }
+        transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
     }
 
     void Update()
     {
         if (!isWaiting && !GameController.instance.failStateTriggered) MoveToWaypoint();
+        
+        
+        _animator.SetBool("isIdle", isWaiting);
     }
 
     private void MoveToWaypoint()
@@ -36,6 +43,14 @@ public class PatrolMovement : MonoBehaviour
             StartCoroutine(WaitAtWaypoint());
         }
         Vector3 moveDirection = (target.position - transform.position).normalized;
+        
+        //set walk animations
+        _animator.SetFloat("velocityX", moveDirection.x);
+        _animator.SetFloat("velocityY", moveDirection.y);
+
+        
+
+        /*
         //transform.rotation = Quaternion.LookRotation(moveDirection);
         // 1. Calculate the direction vector from the current object to the target
         Vector3 directionToTarget = moveDirection;
@@ -47,11 +62,12 @@ public class PatrolMovement : MonoBehaviour
         // 3. Apply the rotation to the Z-axis (assuming your sprite's "forward" is the X-axis/right)
         // Adjust the angle offset if your sprite's default orientation is different (e.g., add or subtract 90 degrees)
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle-180));
-        
+    */
     }
 
     IEnumerator WaitAtWaypoint()
     {
+        transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
         isWaiting = true;
         yield return new WaitForSeconds(waitTime);
         currentWaypointIndex = loopWaypoint
